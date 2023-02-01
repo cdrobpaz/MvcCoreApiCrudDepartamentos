bcb7ee13f46e2154fa60cd69e8e54a8c06551e52
cdrobpaz <cdrobpaz@gmail.com>
2023-02-01 01:44:17 +0100

    Add project files.

---
+++ Controllers/DepartamentosController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreApiCrudDepartamentos.Models;
+using MvcCoreApiCrudDepartamentos.Services;
+
+namespace MvcCoreApiCrudDepartamentos.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private ServiceDepartamentos service;
+        public DepartamentosController(ServiceDepartamentos service)
+        {
+            this.service = service;
+        }
+        public async Task<IActionResult> DepartamentosLocalidad()
+        {
+            List<Departamento> depts = await service.GetDepartamentosAsync();
+            List<string> locs = await service.GetLocalidadesAsync();
+            ViewData["LOCALIDADES"] = locs;
+            return View(depts);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DepartamentosLocalidad(string localidad)
+        {
+            List<Departamento> depts = await service.FindLocalidadAsync(localidad);
+            List<string> locs = await service.GetLocalidadesAsync();
+            ViewData["LOCALIDADES"] = locs;
+            return View(depts);
+        }
+        public async Task<IActionResult> Details(int deptno)
+        {
+            Departamento dept = await service.FindDepartamentoAsync(deptno);
+            return View(dept);
+        }
+
+    }
+}
---
+++ Services/ServiceDepartamentos.cs
@@ -0,0 +1,59 @@
+﻿using MvcCoreApiCrudDepartamentos.Models;
+using System.Net.Http.Headers;
+
+namespace MvcCoreApiCrudDepartamentos.Services
+{
+    public class ServiceDepartamentos
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue header;
+        public ServiceDepartamentos(string url)
+        {
+            this.UrlApi = url;
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+        private async Task<T> GetDatosApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response = await client.GetAsync(request); 
+                if(response.IsSuccessStatusCode)
+                {
+                    T datos = await response.Content.ReadAsAsync<T>();
+                    return datos;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            string request = "/api/departamentos";
+            List<Departamento> empleados = await GetDatosApiAsync<List<Departamento>>(request);
+            return empleados;
+        }
+        public async Task<Departamento> FindDepartamentoAsync(int id)
+        {
+            string request = "/api/departamentos/{id}";
+            Departamento dept = await GetDatosApiAsync<Departamento>(request);
+            return dept;
+        }
+        public async Task<List<Departamento>> FindLocalidadAsync(string localidad)
+        {
+            string request = "/api/departamentos/findlocalidad/" + localidad;
+            List<Departamento> depts = await GetDatosApiAsync<List<Departamento>>(request);
+            return depts;
+        }
+        public async Task<List<string>> GetLocalidadesAsync()
+        {
+            string request = "/api/departamentos/localidades";
+            List<String> localidades = await GetDatosApiAsync<List<String>>(request);
+            return localidades;
+        }
+    }
+}
